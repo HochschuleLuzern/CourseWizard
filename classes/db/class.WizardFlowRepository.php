baff4039120d703de1ad839d7d5a316af045c478
Raphael Heer <raphael.heer@hslu.ch>
2021-01-08 13:48:58 +0100

    Implemented the base of wizard flow model and db-repository

--- classes/db/class.WizardFlowRepository.php
+++ classes/db/class.WizardFlowRepository.php
@@ -2,18 +2,24 @@
 
 namespace CourseWizard\DB;
 
+use CourseWizard\DB\Models\WizardFlow;
+use SebastianBergmann\CodeUnitReverseLookup\Wizard;
+
 class WizardFlowRepository
 {
     const TABLE_NAME = 'rep_robj_xcwi_wizard';
 
-    const COL_ID = 'id';
-    const COL_TARGET_OBJ_ID = 'target_obj_id';
+    //const COL_ID = 'id';
+    const COL_TARGET_REF_ID = 'target_ref_id';
     const COL_EXECUTING_USER = 'executing_user';
     const COL_SELECTED_TEMPLATE = 'selected_template';
     const COL_WIZARD_STATUS = 'wizard_status';
+    const COL_CURRENT_STEP = 'current_step';
 
     /** @var \ilDBInterface */
     protected $db;
+
+    private $cache;
 
     public function __construct(\ilDBInterface $db)
     {
@@ -22,9 +28,104 @@ class WizardFlowRepository
         if(!$db->sequenceExists(self::TABLE_NAME)) {
             $db->createSequence(self::TABLE_NAME);
         }
+
+        $cache = array();
     }
 
-    public function createNewWizardFlow() {
+    private function buildWizardFlowFromRow($row) : WizardFlow
+    {
+        switch($row[self::COL_CURRENT_STEP]) {
+            case WizardFlow::STEP_INTRODUCTION:
+                return WizardFlow::newWizardFlow($row[self::COL_TARGET_REF_ID]);
+            case WizardFlow::STEP_TEMPLATE_SELECTION:
+                return WizardFlow::wizardFlowWithSelectedTemplate(
+                    $row[self::COL_TARGET_REF_ID],
+                    $row[self::COL_SELECTED_TEMPLATE]
+                );
+            case WizardFlow::STEP_CONTENT_INHERITANCE:
+                return WizardFlow::wizardFlowWithContentInheritance(
+                    $row[self::COL_TARGET_REF_ID],
+                    $row[self::COL_SELECTED_TEMPLATE],
+                    null
+                );
+            case WizardFlow::STEP_SELECTED_SETTINGS:
+            case WizardFlow::STEP_FINISHED_WIZARD:
 
+            return WizardFlow::wizardFlowFinished(
+                    $row[self::COL_TARGET_REF_ID],
+                    $row[self::COL_SELECTED_TEMPLATE],
+                    null,
+                    null
+                );
+            default:
+                throw new \InvalidArgumentException("Wizard step with nr {$row[self::COL_CURRENT_STEP]} does not exist");
+        }
+    }
+
+    public function createNewWizardFlow($crs_ref_id) : WizardFlow
+    {
+        $wizard_flow = WizardFlow::newWizardFlow($crs_ref_id);
+
+        $this->db->insert(self::TABLE_NAME,
+            array(
+                self::COL_TARGET_REF_ID => array('integer', $wizard_flow->getCrsRefId()),
+            ));
+    }
+
+    private function queryWizardFlowByCrs($crs_ref_id) : ?WizardFlow
+    {
+        $query = "SELECT * FROM {self::TABLE_NAME} WHERE crs_id = " . $this->db->quote($crs_ref_id, 'integer');
+        $result = $this->db->query($query);
+        if($row = $this->db->fetchAssoc($result)) {
+            $this->cache[$crs_ref_id] = $this->buildWizardFlowFromRow($row);
+            return $this->cache[$crs_ref_id];
+        }
+
+        return null;
+    }
+
+    public function wizardFlowForCrsExists($crs_ref_id) : bool
+    {
+        if(isset($this->cache[$crs_ref_id]) && $this->cache[$crs_ref_id] != null) {
+            return true;
+        } else if($this->queryWizardFlowByCrs($crs_ref_id) != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getWizardFlowForCrs($crs_ref_id) : WizardFlow
+    {
+        if($this->wizardFlowForCrsExists($crs_ref_id)) {
+            if(isset($this->cache[$crs_ref_id])) {
+                return $this->cache[$crs_ref_id];
+            } else {
+                return $this->queryWizardFlowByCrs($crs_ref_id);
+            }
+        } else {
+            return $this->createNewWizardFlow($crs_ref_id);
+        }
+    }
+    
+    public function updateWizardFlow(WizardFlow $wizard_flow) {
+
+        $update = array();
+        $where = array(self::COL_TARGET_REF_ID => array('integer', $wizard_flow->getCrsRefId()));
+
+        switch($wizard_flow->getCurrentStep()) {
+            case WizardFlow::STEP_TEMPLATE_SELECTION:
+                $update = array(
+                    self::COL_SELECTED_TEMPLATE => array('integer', $wizard_flow->getTemplateSelection())
+                );
+        }
+
+        $this->db->insert(
+            self::TABLE_NAME,
+            array(
+                $update,
+                $where
+            )
+        );
     }
 }
--- classes/db/models/class.WizardFlow.php
+++ classes/db/models/class.WizardFlow.php
@@ -4,7 +4,145 @@ namespace CourseWizard\DB\Models;
 
 class WizardFlow
 {
-    public function __construct()
+    public const STATUS_IN_PROGRESS = 1;
+    public const STATUS_POSTPONED = 2;
+    public const STATUS_QUIT = 3;
+    public const STATUS_IMPORTING = 4;
+    public const STATUS_FINISHED = 5;
+
+    public const STEP_INTRODUCTION = 1;
+    public const STEP_TEMPLATE_SELECTION = 2;
+    public const STEP_CONTENT_INHERITANCE = 3;
+    public const STEP_SELECTED_SETTINGS = 4;
+    public const STEP_FINISHED_WIZARD = 5;
+
+    private $crs_ref_id;
+    private $template_selection;
+    private $content_inheritance;
+    private $selected_settings;
+    private $current_status;
+    private $current_step;
+
+    private function __construct($crs_ref_id, $template_selection, $content_inheritance, $selected_settings, $current_status, $current_step)
     {
+        $this->crs_ref_id = $crs_ref_id;
+        $this->template_selection = $template_selection;
+        $this->content_inheritance = $content_inheritance;
+        $this->selected_settings = $selected_settings;
+        $this->current_status = $current_status;
+        $this->current_step = $current_step;
+    }
+
+    public static function newWizardFlow($crs_ref_id) {
+        return new self(
+            $crs_ref_id,
+            null,
+            null,
+            null,
+            self::STATUS_IN_PROGRESS,
+            self::STEP_INTRODUCTION
+        );
+    }
+
+    public static function wizardFlowWithSelectedTemplate($crs_ref_id, $template_selection) {
+        return new self(
+            $crs_ref_id,
+            $template_selection,
+            null,
+            null,
+            self::STATUS_IN_PROGRESS,
+            self::STEP_CONTENT_INHERITANCE
+        );
+    }
+
+    public static function wizardFlowWithContentInheritance($crs_ref_id, $template_selection, $content_inheritance) {
+        return new self(
+            $crs_ref_id,
+            $template_selection,
+            $content_inheritance,
+            null,
+            self::STATUS_IN_PROGRESS,
+            self::STEP_CONTENT_INHERITANCE
+        );
+    }
+
+    public static function wizardFlowFinished($crs_ref_id, $template_selection, $content_inheritance, $selected_settings) {
+        return new self(
+            $crs_ref_id,
+            $template_selection,
+            $content_inheritance,
+            $selected_settings,
+            self::STATUS_FINISHED,
+            self::STEP_FINISHED_WIZARD
+        );
+    }
+
+    public function withSelectedTemplate($selected_template) {
+        $clone = clone $this;
+        $clone->template_selection = $selected_template;
+        $clone->current_step = self::STEP_CONTENT_INHERITANCE;
+        return $clone;
+    }
+
+    public function withSelectedInheritance($content_inheritance) {
+        $clone = clone $this;
+        $clone->content_inheritance = $content_inheritance;
+        $clone->current_step = self::STEP_SELECTED_SETTINGS;
+        return $clone;
+    }
+
+    public function withSelectedSettings($selected_settings) {
+        $clone = clone $this;
+        $clone->selected_settings = $selected_settings;
+        $clone->current_step = self::STEP_FINISHED_WIZARD;
+        return $clone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCrsRefId()
+    {
+        return $this->crs_ref_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTemplateSelection()
+    {
+        return $this->template_selection;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContentInheritance()
+    {
+        return $this->content_inheritance;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSelectedSettings()
+    {
+        return $this->selected_settings;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCurrentStatus()
+    {
+        return $this->current_status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCurrentStep()
+    {
+        return $this->current_step;
     }
 }
