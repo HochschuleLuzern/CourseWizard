bbab8d996c04e937c831152c11efc4266914f604
Raphael Heer <raphael.heer@hslu.ch>
2021-04-27 16:33:45 +0200

    Changed required ILIAS Version for Plugin

--- plugin.php
+++ plugin.php
@@ -3,6 +3,6 @@
 $id = "xcwi";
 $version = "0.2.7";
 $ilias_min_version = "6";
-$ilias_max_version = "6.9";
+$ilias_max_version = "7.9";
 $responsible = "Raphael Heer";
 $responsible_mail = "raphael.heer@hslu.ch";
