3b21217515e6f1ea22f556bc07d5f763ddd81d92
Raphael <raphael.heer@hslu.ch>
2022-05-04 10:24:05 +0200

    Fix ILIAS-Max-Version

--- plugin.php
+++ plugin.php
@@ -3,6 +3,6 @@
 $id = "xcwi";
 $version = "0.3.0";
 $ilias_min_version = "6";
-$ilias_max_version = "7.9";
+$ilias_max_version = "7.99";
 $responsible = "Raphael Heer";
 $responsible_mail = "raphael.heer@hslu.ch";
