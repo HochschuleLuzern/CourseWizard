6b424ef687493c4295e79b3825933c1d8b8f3bb2
msalterhslu <mark.salter@hslu.ch>
2025-04-15 15:19:37 +0200

    Compatibility with ILIAS 9

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 
 $id = "xcwi";
-$version = "0.5.0";
-$ilias_min_version = "6";
-$ilias_max_version = "8.99";
+$version = "0.5.1";
+$ilias_min_version = "8.0";
+$ilias_max_version = "9.999";
 $responsible = "Mark Salter";
 $responsible_mail = "mark.salter@hslu.ch";
