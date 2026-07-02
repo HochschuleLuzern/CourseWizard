4dd6304268843d514e910fe6092f09f868a6b5b7
Katalin Lukacsne Toth <katalin.lukacsne@hslu.ch>
2026-07-02 14:47:34 +0200

    Adjust component path
    Ilias 10 moved the file our class was copied from
    ilTable2GUI is deprecated but works for now

--- classes/wizard_modal/custom_ui/ContentInheritanceTableGUI.php
+++ classes/wizard_modal/custom_ui/ContentInheritanceTableGUI.php
@@ -44,7 +44,7 @@ class ContentInheritanceTableGUI extends \ilTable2GUI
 
         $this->setEnableHeader(true);
         $this->setFormAction($this->ctrl->getFormAction($this->getParentObject()));
-        $this->setRowTemplate("tpl.obj_copy_selection_row.html", "Services/Object");
+        $this->setRowTemplate("tpl.obj_copy_selection_row.html", "components/ILIAS/ILIASObject");
         $this->setEnableTitle(true);
         $this->setEnableNumInfo(true);
         $this->setLimit(999999);
